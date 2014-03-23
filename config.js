c0c613e3d42db83b8fc2d0c6ec297ab764fc0a68
harishekhar <shekhardtu@gmail.com>
2014-03-23 11:37:32 +0530

    Complete app

--- config.js
+++ config.js
@@ -1,1 +1,33 @@
-j
+/**
+ * This file runs some configuration settings on your express application. 
+ */
+ 
+ // Include the handlebars templating library
+ 
+ var handlebars =require('express3-handlebars'),
+ express =require('express');
+ 
+ // Require() -ing this module will return a function 
+ // that the index.js file will use to configure the 
+ // express application
+ 
+ module.exports =function(app){
+ 
+	// Register and configure the handle bars templating engine 
+	app.engine('html', handlebars({
+		defaultLayout: 'main', 
+		extname: ".html",
+		layoutDir:__dirname+ '/views/layouts'
+		}));
+		//Set .html as the default template extension 
+		app.set('view engine', 'html');
+		// Tell express where it can find the templates 
+		app.set('views', __dirname+ '/views');
+		
+		// Make the files in the public folder avilable to the world
+		app.use(express.static(__dirname + '/public'));
+		
+		// Parse POST request data. It will be available in the req.body object
+		app.use(express.urlencoded());
+		};
+		
--- readme.txt
+++
@@ -1,1 +0,0 @@
-It is an experiment with node.js 
